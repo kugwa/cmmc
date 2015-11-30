3914e568ee28efd4e6fb18d6a1070c2c47d69894
Ting-Wei Lan <lantw44@gmail.com>
2015-11-30 15:51:45 +0800

    Use strerror_r to make error messages more useful
    This commit adds a new file, common.h, which contains private macros
    and static functions to make the code cleaner.

---
+++ src/common.h
@@ -0,0 +1,52 @@
+#ifndef CCMMC_HEADER_COMMON_H
+#define CCMMC_HEADER_COMMON_H
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// The name of this program, which can be used in error messages
+#define name ccmmc_main_name
+extern const char *ccmmc_main_name;
+
+// Calculate the length of an array
+#define SIZEOF_ARRAY(x) (sizeof(x)/sizeof(x[0]))
+
+// Make sure that we have a POSIX-compatible strerror_r function
+#ifndef HAVE_STRERROR_R
+# error "strerror_r() is not available"
+#endif
+#ifndef HAVE_DECL_STRERROR_R
+# error "strerror_r() is not declared"
+#endif
+#ifdef STRERROR_R_CHAR_P
+# ifdef _GNU_SOURCE
+#  error "_GNU_SOURCE must not be defined"
+# else
+#  error "This program doesn't work with GNU-specific strerror_r()"
+# endif
+#endif
+
+// Make multi-thread error reporting easier
+#define ERR_LEN     1024
+#define ERR_DECL    char err[ERR_LEN]
+#define ERR_MSG     get_err_msg(errno, err, ERR_LEN)
+
+static char *get_err_msg(int num, char *buf, size_t len) {
+    int errno_save = errno;
+    if (strerror_r (num, buf, len) != 0)
+        snprintf (buf, len, "Unknown error %d", num);
+    errno = errno_save;
+    return buf;
+}
+
+#define ERR_FATAL_CHECK(result, function) \
+    if (result == NULL) { \
+        ERR_DECL; \
+        fprintf(stderr, "%s: " #function ": %s\n", name, ERR_MSG); \
+        exit(1); \
+    }
+
+#endif
+// vim: set sw=4 ts=4 sts=4 et:
--- src/main.c
+++ src/main.c
@@ -3,25 +3,40 @@
 #endif
 
 #include "ast.h"
+#include "common.h"
 #include "libparser_a-parser.h"
 
+#include <errno.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 extern FILE *yyin;
 extern AST_NODE *prog;
 
+const char *ccmmc_main_name;
+
 int main (int argc, char **argv)
 {
+    ERR_DECL;
+    setlocale (LC_ALL, "");
+    name = strrchr (argv[0], '/');
+    name = name == NULL ? name : name + 1;
+
     if (argc != 2) {
-        fputs("usage: parser [source file]\n", stderr);
+        fprintf(stderr, "Usage: %s SOURCE\n", name);
         exit(1);
     }
-    yyin = fopen(argv[1],"r");
+
+    const char *filename = argv[1];
+    yyin = fopen(filename, "r");
     if (yyin == NULL) {
-        fputs("Error opening source file.\n", stderr);
+        fprintf(stderr, "%s: %s: %s\n", name, filename, ERR_MSG);
         exit(1);
     }
+
     yyparse();
     printGV(prog, NULL);
     return 0;
