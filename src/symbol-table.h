21e57e72f5d7417bc2704d0356d66c21c269114a
Ting-Wei Lan <lantw44@gmail.com>
2015-12-06 22:44:02 +0800

    Add a flag to mark whether the type is a function

--- src/symbol-table.h
+++ src/symbol-table.h
@@ -9,8 +9,8 @@
 #define CCMMC_SYMBOL_SCOPE_HASH_TABLE_SIZE 256
 
 typedef enum CcmmcSymbolKind_enum {
-    CCMMC_SYMBOL_KIND_VARIABLE,
     CCMMC_SYMBOL_KIND_TYPE,
+    CCMMC_SYMBOL_KIND_VARIABLE,
     CCMMC_SYMBOL_KIND_FUNCTION
 } CcmmcSymbolKind;
 
@@ -19,6 +19,7 @@ typedef struct CcmmcSymbolType_struct {
     CcmmcAstValueType type_base;
     size_t array_dimension;
     size_t *array_size;
+    bool param_valid;
     size_t param_count;
     CcmmcSymbolType *param_list;
 } CcmmcSymbolType;
@@ -49,11 +50,23 @@ typedef struct CcmmcSymbolTable_struct {
     CcmmcSymbolScope *current;
 } CcmmcSymbolTable;
 
+static inline bool ccmmc_symbol_type_is_scalar(CcmmcSymbolType type) {
+    return type.array_dimension == 0 && !type.param_valid;
+}
+static inline bool ccmmc_symbol_type_is_array(CcmmcSymbolType type) {
+    return type.array_dimension != 0;
+}
+static inline bool ccmmc_symbol_type_is_function(CcmmcSymbolType type) {
+    return type.param_valid;
+}
 static inline bool ccmmc_symbol_is_scalar(CcmmcSymbol *symbol) {
-    return symbol->type.array_dimension == 0;
+    return ccmmc_symbol_type_is_scalar(symbol->type);
 }
 static inline bool ccmmc_symbol_is_array(CcmmcSymbol *symbol) {
-    return symbol->type.array_dimension != 0;
+    return ccmmc_symbol_type_is_array(symbol->type);
+}
+static inline bool ccmmc_symbol_is_function(CcmmcSymbol *symbol) {
+    return ccmmc_symbol_type_is_function(symbol->type);
 }
 
 void             ccmmc_symbol_table_open_scope      (CcmmcSymbolTable *table);
